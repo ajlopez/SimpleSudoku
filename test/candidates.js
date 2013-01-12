2a6a8b20c3252da52e06bdf0670144f0123bb73e
ajlopez <webmaster@ajlopez.com>
2013-01-12 11:47:21 -0300

    Fix associated positions calculation, with tests

--- test/candidates.js
+++ test/candidates.js
@@ -15,3 +15,27 @@ for (var x = 0; x < 9; x++)
         for (var k = 1; k <= 9; k++)
             assert.ok(candidates.indexOf(k) >= 0);
     }
+
+// Put a number
+
+game.setNumber(1, 1, 1);
+
+var result = game.getCandidates(1, 1);
+assert.ok(result);
+assert.equal(result.length, 8);
+assert.ok(result.indexOf(1) < 0);
+
+var result = game.getCandidates(0, 0);
+assert.ok(result);
+assert.equal(result.length, 8);
+assert.ok(result.indexOf(1) < 0);
+
+var result = game.getCandidates(8, 0);
+assert.ok(result);
+assert.equal(result.length, 8);
+assert.ok(result.indexOf(1) < 0);
+
+var result = game.getCandidates(0, 8);
+assert.ok(result);
+assert.equal(result.length, 8);
+assert.ok(result.indexOf(1) < 0);
--- test/positions.js
+++ test/positions.js
@@ -8,7 +8,7 @@ for (var x = 0; x < 9; x++)
     for (var y = 0; y < 9; y++) {
         var positions = simplesudoku.getPositions(x, y);
         assert.ok(positions);
-        assert.equal(positions.length, 8+8+8);
+        assert.equal(positions.length, 8+8+4);
 
         positions.forEach(function (position) {
             assert.ok(position.x !== undefined);
@@ -16,4 +16,31 @@ for (var x = 0; x < 9; x++)
             assert.ok(position.x !== x || position.y !== y);
         });
     }
+
+// Contains position
+
+function contains(positions, position) {
+    for (var n in positions) {
+        var pos = positions[n];
+
+        if (pos.x === position.x && pos.y === position.y)
+            return true;
+    }
+
+    return false;
+}
+
+var positions = simplesudoku.getPositions(0, 0);
+
+for (var x = 0; x < 3; x++)
+    for (var y = 0; y < 3; y++)
+        if (x !== 0 || y !== 0)
+            assert.ok(contains(positions, { x: x, y: y }));
+
+var positions = simplesudoku.getPositions(2, 2);
+
+for (var x = 0; x < 3; x++)
+    for (var y = 0; y < 3; y++)
+        if (x !== 2 || y !== 2)
+            assert.ok(contains(positions, { x: x, y: y }));
 
