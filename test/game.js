7ef1438fefd659967036e8c8de7a278031239afc
ajlopez <webmaster@ajlopez.com>
2013-01-12 11:00:08 -0300

    getNumber first test

--- test/game.js
+++ test/game.js
@@ -7,3 +7,10 @@ var simplesudoku = require('..'),
 var game = simplesudoku.createGame();
 
 assert.ok(game);
+
+// Is empty
+
+for (var x = 0; x < 9; x++)
+    for (var y = 0; y < 9; y++)
+        assert.equal(game.getNumber(x, y), null);
+
