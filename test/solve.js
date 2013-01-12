e8fe90f8818c1378eb1bde5fc1ed932de1f1418a
ajlopez <webmaster@ajlopez.com>
2013-01-12 16:13:11 -0300

    Solve second test

--- test/solve.js
+++ test/solve.js
@@ -2,7 +2,10 @@
 var simplesudoku = require('..'),
     assert = require('assert');
 
-// Solve a problem
+// Problems from http://magictour.free.fr/sudoku.htm
+// http://magictour.free.fr/topn87
+
+// Solve problem 1
 
 var game = simplesudoku.createGame();
 game.load("4...3.......6..8..........1....5..9..8....6...7.2........1.27..5.3....4.9........");
@@ -11,3 +14,11 @@ var solution = game.solve();
 assert.ok(solution);
 assert.equal(solution.getCount(), 81);
 
+// Solve problem 2
+
+var game = simplesudoku.createGame();
+game.load("7.8...3.....2.1...5.........4.....263...8.......1...9..9.6....4....7.5...........");
+var solution = game.solve();
+
+assert.ok(solution);
+assert.equal(solution.getCount(), 81);
