d38d658f309395612db2d9f38277b1b8f91d925b
ajlopez <webmaster@ajlopez.com>
2013-01-12 17:05:51 -0300

    New problem files

--- samples/solvefile/README.md
+++ samples/solvefile/README.md
@@ -18,5 +18,10 @@ node run problems01.txt
 ## Files
 Taken from [Guenter Stertenbrink's Sudoku page](http://magictour.free.fr/sudoku.htm)
 
-- problems01: [http://magictour.free.fr/topn87].
+- problems01.txt: http://magictour.free.fr/topn87
+- problems02.txt: http://magictour.free.fr/top95
+- problems03.txt: http://magictour.free.fr/topn234
+- problems04.txt: http://magictour.free.fr/top1465
+- problems05.txt: http://magictour.free.fr/top100
+
 
--- samples/solvefile/run.js
+++ samples/solvefile/run.js
@@ -8,12 +8,14 @@ var filenames = process.argv.slice(2);
 filenames.forEach(function (filename) {
     var text = fs.readFileSync(filename).toString();
     
-    for (var pos = text.indexOf('\n'); pos >= 0; text = text.substring(pos + 1)) {
+    for (var pos = text.indexOf('\n'); text && pos >= 0; text = text.substring(pos + 1)) {
         var line = text.substring(0, pos);
         if (!line)
             continue;
         var game = simplesudoku.createGame();
         game.load(line);
+        if (game.getCount() <= 10)
+            continue;
         console.log(game.asString());
         var solution = game.solveSync();
         if (solution)
